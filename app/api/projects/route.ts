b1c538cf0ad3d9067d6c6898f7253e466cc7beb8
Javkhlant <javkhlant.altankhuyag@gmail.com>
2026-01-06 16:18:03 +0800

    fix project filter

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -7,7 +7,7 @@ export async function GET() {
     const { data: projects } = (await client
         .from("projects")
         .select()
-        .is("status", "published")
+        .eq("status", "published")
         .order("order", { ascending: true })) as { data: Project[] | null };
 
     return NextResponse.json(projects);
